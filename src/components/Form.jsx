f08964c09837b4e93ae650c33df9bc860aee2f6b
Elias <kyosaki809@gmail.com>
2025-09-20 13:20:47 +0200

    ✨ F01: LOGIN > Add title and form

---
+++ src/components/Form.jsx
@@ -0,0 +1,33 @@
+import { useState } from "react"
+
+export function Form() {
+
+const [inputAss, setInputValue] = useState('')
+
+
+
+
+const handleChange = (e) => {
+
+    setInputValue(e.target.value)
+}
+
+
+const handleSubmit = (e) => { 
+    e.preventDefault()
+    alert(`Bienvenue ${inputAss}`)
+    setInputValue('')
+}
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label htmlFor="name">Name:</label>
+      <input type="text" 
+      value={inputAss} 
+      onChange={handleChange} 
+      
+      placeholder="Entrez votre prénom..." required/>
+        <button>Accéder à votre espace</button>  
+    </form>
+  )
+}   
