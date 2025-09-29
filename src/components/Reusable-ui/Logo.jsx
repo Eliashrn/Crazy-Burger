b5839b3c6dd7cf25ec86f8f9d29b98cefe09948f
Elias <kyosaki809@gmail.com>
2025-09-29 12:14:18 +0200

    💄 F03: LOGO > Add styling + fonts

--- src/components/Reusable-ui/Logo.jsx
+++ src/components/Reusable-ui/Logo.jsx
@@ -2,9 +2,38 @@ import React from "react";
 import styled from "styled-components";
 
 export default function Logo() {
-  return <LogoStyled>Logo Crazy burger</LogoStyled>;
+  return (
+    <LogoStyled>
+      <h2>Crazee</h2>
+      <img src="./image/F03 logo-orange.png" alt="Logo Crazy Burger" />
+      <h2>Burger</h2>
+    </LogoStyled>
+  );
 }
 
 const LogoStyled = styled.div`
-  background: blue;
+  color: #ffa01b;
+  display: flex;
+  align-items: center;
+  transform: scale(2.5);
+
+  h2 {
+    display: inline;
+    text-align: center;
+    color: #ffa01b;
+    font-size: 36px;
+    line-height: 1em;
+    font-weight: 700;
+    text-transform: uppercase;
+    letter-spacing: 1.5px;
+    font-family: "Amatic SC", cursive;
+  }
+
+  img {
+    object-fit: contain;
+    object-position: center;
+    height: 60px;
+    width: 80px;
+    margin: 0 5px;
+  }
 `;
