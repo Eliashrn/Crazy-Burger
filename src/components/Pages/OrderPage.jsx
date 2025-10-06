2e0157ae6df039ed72be0ba0f1eb91e80a6df934
Elias <kyosaki809@gmail.com>
2025-10-06 11:32:43 +0200

    💄 F04: ORDERPAGE > Update OrderPageStyled background color to use theme colors for consistency

--- src/components/Pages/OrderPage.jsx
+++ src/components/Pages/OrderPage.jsx
@@ -20,7 +20,7 @@ const OrderPageStyled = styled.div`
   box-sizing: border-box;
   margin: 0;
   padding: 0;
-  background: orange;
+  background: ${theme.colors.primary};
   display: flex;
   justify-content: center;
   align-items: center;
