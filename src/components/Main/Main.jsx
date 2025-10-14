cd00eace8841e40f0be98385c251cb4fca10e882
Elias <kyosaki809@gmail.com>
2025-10-14 13:34:25 +0200

    💄 F07: MAIN > Add layout with Admin

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -3,28 +3,42 @@ import { theme } from "../../theme";
 import Menu from "./Menu";
 
 export default function Main() {
-  // const [products, setProducts] = useState(fakeMenu1);
-
   return (
-    <MainStyled>
+    <MainStyled className="main">
       {/* <div className="basket">basket</div> */}
-      <Menu />
+      <div className="menu-and-admin">
+        <Menu />
+        <div className="admin">admin</div>
+      </div>
     </MainStyled>
   );
 }
 
 const MainStyled = styled.div`
-  height: 85vh;
+  border: 3px solid green;
   border-bottom-left-radius: ${theme.borderRadius.extraRound};
   border-bottom-right-radius: ${theme.borderRadius.extraRound};
   box-shadow: 0px 8px 20px 8px rgba(0, 0, 0, 0.2) inset;
 
   display: grid;
   grid-template-columns: 1fr;
-  overflow: hidden;
 
-  /* 
-  .basket {
+  /* .basket {
     background: pink;
   } */
+  .menu-and-admin {
+    position: relative;
+    overflow-y: hidden;
+    display: grid;
+    border-bottom-left-radius: ${theme.borderRadius.extraRound};
+    border-bottom-right-radius: ${theme.borderRadius.extraRound};
+    .admin {
+      background: red;
+      height: 250px;
+      bottom: 0;
+      left: 0;
+      right: 0;
+      position: absolute;
+    }
+  }
 `;
