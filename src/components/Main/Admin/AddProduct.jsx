58639970dd589f166d486d882a0f10dffb5e0c6f
Elias <kyosaki809@gmail.com>
2025-10-21 12:56:21 +0200

    ✨ F08: ADD > input on AddProduct

--- src/components/Main/Admin/AddProduct.jsx
+++ src/components/Main/Admin/AddProduct.jsx
@@ -1,11 +1,38 @@
 import React from "react";
+import Input from "../../Reusable-ui/Input";
+import { PiHamburgerFill } from "react-icons/pi";
+import { MdPhotoCamera } from "react-icons/md";
+import { LuEuro } from "react-icons/lu";
+import styled from "styled-components";
+import { theme } from "../../../theme";
 
 export default function AddProduct() {
   return (
-    <form>
-      <input type="text" placeholder="Nom du produit" />
-      <input type="text" placeholder="Description du produit" />
-      <input type="text" placeholder="Prix " />
-    </form>
+    <FormStyled>
+      <div className="left">
+        <div className="img-container">
+          <img src="" alt="" />
+        </div>
+      </div>
+      <div className="right-side">
+        <Input
+          placeholder="Nom du produit (ex: Super Burger)"
+          Icon={<PiHamburgerFill className="icon" />}
+        />
+        <Input
+          placeholder="Lien URL d'une image (ex: https://la-photo-de-mon-produit.png)"
+          Icon={<MdPhotoCamera className="icon" />}
+        />
+        <Input placeholder="Prix" Icon={<LuEuro className="icon" />} />
+      </div>
+    </FormStyled>
   );
 }
+
+const FormStyled = styled.form`
+  .right-side {
+    background: #fff;
+    border-radius: 5px;
+    display: grid;
+  }
+`;
--- src/components/Reusable-ui/Input.jsx
+++ src/components/Reusable-ui/Input.jsx
@@ -9,31 +9,28 @@ export default function Input({ value, onChange, Icon, ...extraProps }) {
   );
 }
 
-const InputStyled = styled.div` 
-    background: #fff;
-    border-radius: 5px;
-    display: flex;
-    align-items: center;
-    padding: 18px 24px;
-    margin: 18px 0;
+const InputStyled = styled.div`
+  background: #fff;
+  border-radius: 5px;
+  display: flex;
+  align-items: center;
+  padding: 18px 24px;
+  margin: 18px 0;
 
-    .icon {
-      font-size: 15px;
-      margin-right: 8px;
-      color: #93a2b1;
-    }
+  .icon {
+    font-size: 15px;
+    margin-right: 8px;
+    color: #93a2b1;
+  }
 
-    input {
-      border: none;
-      font-size: 15px;
-      color: #17161a;
-      width: 100%;
-
-      &placeholder {
-      background: white;
-      color: lightgrey;
-    }
-
-  
-  
+  input {
+    border: none;
+    font-size: 15px;
+    color: #17161a;
+    width: 100%;
+  }
+  &::placeholder {
+    background: white;
+    color: lightgrey;
+  }
 `;
