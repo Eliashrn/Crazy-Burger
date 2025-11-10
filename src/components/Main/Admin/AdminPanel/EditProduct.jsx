8ece8cadc186cec15722db23c9b1de44ff5df456
Elias <kyosaki809@gmail.com>
2025-11-10 11:38:35 +0100

    💄F09:  ADMIN > EDITFORM > Add HintPMessage

--- src/components/Main/Admin/AdminPanel/EditProduct.jsx
+++ src/components/Main/Admin/AdminPanel/EditProduct.jsx
@@ -1,5 +1,10 @@
 import React from "react";
+import HintMessage from "./HintMessage";
 
 export default function EditProduct() {
-  return <div>zozo</div>;
+  return (
+    <div>
+      <HintMessage />
+    </div>
+  );
 }
