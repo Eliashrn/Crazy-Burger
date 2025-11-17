4ade169f0887b1d2c1e28f8893256b63c2b6a2bd
Elias <kyosaki809@gmail.com>
2025-11-17 10:44:50 +0100

    ♻️F09: ADMIN > EditProduct > Extract EditInfoMessage

--- src/components/Main/Admin/AdminPanel/EditProduct.jsx
+++ src/components/Main/Admin/AdminPanel/EditProduct.jsx
@@ -4,7 +4,7 @@ import styled from "styled-components";
 import ImagePreview from "./ImagePreview";
 import Input from "../../../Reusable-ui/Input";
 import { getInputTextConfig } from "./getInputTextConfig";
-import { theme } from "../../../../theme";
+import EditInfoMessage from "./EditInfoMessage";
 
 export default function EditProduct() {
   const {
@@ -41,11 +41,8 @@ export default function EditProduct() {
         ))}
       </div>
       <div className="submit">
-        <span className="sentence">
-        Cliquer sur un produit du menu pour le modifier {""}
-        <span className="live-update">en temps réel</span>
-        </span>
-        </div>
+        <EditInfoMessage />
+      </div>
     </EditProductStyled>
   );
 }
@@ -70,13 +67,5 @@ const EditProductStyled = styled.form`
     display: flex;
     position: relative;
     align-items: center;
-  }
-
-  .sentence {
-    color: ${theme.colors.primary};
-    font-size: ${theme.fonts.size.SM};
-    .live-update {
-      text-decoration: underline;
-    }
   }
 `;
