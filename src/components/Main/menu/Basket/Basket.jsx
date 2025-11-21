f72b1102b5d0493703d631b9de4febc6907d4299
Elias <kyosaki809@gmail.com>
2025-11-21 13:15:33 +0100

    💄F11: BASKET > ADD BASKETCard

--- src/components/Main/menu/Basket/Basket.jsx
+++ src/components/Main/menu/Basket/Basket.jsx
@@ -19,14 +19,28 @@ export default function Basket() {
       <Header>
         <Total amoundTopay={formatPrice(0)} />
       </Header>
-      {isBasketEmpty ? <EmptyBasket /> : <BasketProduct />}
+      {isBasketEmpty ? <EmptyBasket /> : <BasketProduct basket={basket} />}
       <Footer />
     </BasketStyled>
   );
 }
 
 const BasketStyled = styled.div`
+  background: ${theme.colors.background_white};
+  box-shadow: ${theme.shadows.basket};
   display: flex;
   flex-direction: column;
   border-bottom-left-radius: ${theme.borderRadius.extraRound};
+  height: 85vh;
+
+  .header {
+    position: sticky;
+    top: 0;
+  }
+
+  .footer {
+    border-bottom-left-radius: ${theme.borderRadius.extraRound};
+    position: sticky;
+    bottom: 0;
+  }
 `;
--- src/components/Main/menu/Basket/BasketProduct.jsx
+++ src/components/Main/menu/Basket/BasketProduct.jsx
@@ -1,5 +1,34 @@
 import React from "react";
+import styled from "styled-components";
+import BasketCard from "./BasketCard";
 
-export default function BasketProduct() {
-  return <div>BasketProduct</div>;
+export default function BasketProduct({ basket }) {
+  return (
+    <BasketProductStyled>
+      {basket.map((basketProduct) => (
+        <div className="basket-card">
+          <BasketCard key={basketProduct.id} {...basketProduct} />
+        </div>
+      ))}
+    </BasketProductStyled>
+  );
 }
+
+const BasketProductStyled = styled.div`
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  overflow-y: scroll;
+
+  .basket-card {
+    margin: 10px 36px;
+    height: 85px;
+    box-sizing: border-box;
+    &:first-child {
+      margin-top: 20px;
+    }
+    &:last-child {
+      margin-bottom: 20px;
+    }
+  }
+`;
